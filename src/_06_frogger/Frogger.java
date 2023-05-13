c83cea078aaaf0fae9c9f3d7f5b2b93789091a91
league <league@imac-29.attlocal.net>
2023-05-13 12:59:41 -0700

    frogger

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,10 +3,23 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 800;
-    static final int HEIGHT = 600;
-    int x=400;
-    int y=550;
+    static final int WIDTH = 1000;
+    static final int HEIGHT = 800;
+    int x=500;
+    int y=675;
+    Car c1;
+    Car c2;
+    Car c3;
+    Car c4;
+    boolean intersects(Car car) {
+    	 if ((y > car.getY() && y < car.getY()+50) &&
+    	                (x > car.getX() && x < car.getX()+car.getSize())) {
+    	   return true;
+    	  }
+    	 else  {
+    	  return false;
+    	 }
+    }
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -14,14 +27,32 @@ public class Frogger extends PApplet {
 
     @Override
     public void setup() {
-
+    	c1 = new Car((int)random (0,WIDTH), (int)random (0, HEIGHT-150), 85, (int)random (2,12));
+    	c2 = new Car((int)random (0,WIDTH), (int)random (0, HEIGHT-150), 140, (int)random (2,12));
+    	c3 = new Car((int)random (0,WIDTH), (int)random (0, HEIGHT-150), 120, (int)random (2,12));
+    	c4 = new Car((int)random (0,WIDTH), (int)random (0, HEIGHT-150), 100, (int)random (2,12));
     }
 
     @Override
     public void draw() {
     	background(0,0,255);
     	fill(0, 255, 0);
-    	ellipse(x, y, 75, 65);
+    	ellipse(x, y, 60, 45);
+    	fill(255,0,0);
+    	c1.display();
+    	c2.display();
+    	c3.display();
+    	c4.display();
+    	c1.moveLeft();
+    	c2.moveLeft();
+    	c3.moveRight();
+    	c4.moveRight();
+    	if(intersects(c1)||intersects(c2)||intersects(c3)||intersects(c4)) {
+    		x = 500;
+    		y = 675;
+    	}
+    	
+    
     }    
     public void keyPressed()
     {
@@ -49,10 +80,56 @@ public class Frogger extends PApplet {
     		return true;
     	}else if(y<0 || y>HEIGHT) {
     		return true;
-    	}
+    	} return false;
     }
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
         
+    }
+    
+    class Car {
+    	int xCar;
+    	int yCar;
+    	int size;
+    	int speed;
+    	Car(int x, int y, int size, int speed) {			
+    		this.xCar = x;
+    		this.yCar = y;
+    		this.size = size;
+    		this.speed = speed;
+    	}
+    	void moveLeft() {
+    		xCar -= speed;
+    		if(xCar<0-size) {
+    			xCar = WIDTH; 
+    			yCar = (int)random (0, HEIGHT-150);
+    			speed = (int)random (2,12);
+    		}
+    	}
+    	void moveRight() {
+    		xCar += speed;
+    		if(xCar>WIDTH+size) {
+    			xCar = 0-size;
+    			yCar = (int)random (0, HEIGHT-150);
+    			speed = (int)random (2,12);
+    		}
+    	}
+    	
+    	int getX() {
+    		return xCar;
+    	}
+    	
+    	int getY() {
+    		return yCar;
+    	}
+    	
+    	int getSize() {
+    		return size;
+    	}
+    	
+    	void display() {
+    		fill(255,0,0);
+    		rect(xCar , yCar,  size, 50);
+    	}
     }
 }
